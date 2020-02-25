c899b1f953aee8c3521ba65a67a500cea83f2335
smwbtech <smwb.tech@gmail.com>
2020-02-25 21:31:58 +0300

    Update vue-picture-element.d.ts

--- src/types/vue-picture-element.d.ts
+++ src/types/vue-picture-element.d.ts
@@ -1,15 +1,15 @@
-declare module 'vue-picture-element' {
-  export interface MediaQuery {
-    [key: string]: [string]
-  }
-
-  export interface SettingItem {
-    media?: MediaQuery[]
-    xDelimetr?: string[]
-    test?: RegExp
-  }
-
-  export interface Settings {
-    [key: string]: SettingItem
-  }
+declare interface MediaQuery {
+  [key: string]: [string]
 }
+
+declare interface SettingItem {
+  media?: MediaQuery[]
+  delimetrs?: string[]
+  test?: RegExp
+}
+
+declare interface Settings {
+  [key: string]: SettingItem
+}
+
+declare type DelimeterType = 'x' | 'w' | undefined
