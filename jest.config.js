d8742800b52422db9fb9f509d69b8cda46526b6d
smwbtech <smwb.tech@gmail.com>
2020-03-02 17:04:35 +0300

    Unit tests complete
    *Added: unit tests
    *Added: coveralls package

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/src/components/**',
+    '!**/src/components/index.js',
+    '!**/src/components/index.ts',
+    '!**/App.{vue}',
+    '!**/wrapper.js',
+    '!**/node_modules/**',
+    '!<rootDir>/dist/**',
+    '!<rootDir>/src/plugins/**',
+    '!<rootDir>/tests/unit/**'
+  ],
+  coverageReporters: ['lcov', 'text-summary']
 }
--- tests/unit/vue-picture-element.spec.ts
+++ tests/unit/vue-picture-element.spec.ts
@@ -7,7 +7,7 @@ const settings = {
     delimeters: ['2x', '3x']
   },
   small: {
-    media: { 'max-width': '200px', orientation: 'landscape' },
+    media: { 'min-width': '300px', orientation: 'portrait' },
     delimeters: ['', '200w', '400w']
   },
   /* eslint-disable */
@@ -54,7 +54,7 @@ describe('Testing VuePictureElement.ts', () => {
     expect(cmp.isVueInstance()).toBeTruthy()
   })
 
-  describe('Testing rendering', () => {
+  describe('Testing rendering with settings', () => {
     it('Should render picture element', () => {
       expect(cmp.is('picture')).toBeTruthy()
     })
@@ -87,6 +87,123 @@ describe('Testing VuePictureElement.ts', () => {
           '/img/image-large@2x.jpg 2x, /img/image-large@3x.jpg 3x'
         )
       })
+
+      it('Should have media attribute eq "(max-width: 200px) and (orientation: landscape)"', () => {
+        expect(sourceElWebp.attributes('media')).toEqual(
+          '(max-width: 200px) and (orientation: landscape)'
+        )
+        expect(sourceElPng.attributes('media')).toEqual(
+          '(max-width: 200px) and (orientation: landscape)'
+        )
+        expect(sourceElJpg.attributes('media')).toEqual(
+          '(max-width: 200px) and (orientation: landscape)'
+        )
+      })
+    })
+
+    describe('Testing "small" image', () => {
+      let sourceElWebp: Wrapper<Vue>
+      let sourceElPng: Wrapper<Vue>
+      let sourceElJpg: Wrapper<Vue>
+
+      beforeEach(() => {
+        sourceElWebp = cmp.find(
+          'source[srcset*="/img/image-small-400.webp 400w"]'
+        )
+        sourceElPng = cmp.find(
+          'source[srcset*="/img/image-small-400.png 400w"]'
+        )
+        sourceElJpg = cmp.find(
+          'source[srcset*="/img/image-small-400.jpg 400w"]'
+        )
+      })
+
+      it('Should have srcset eq "/img/image-small.[ext], /img/image-small-200.[ext] 200w, /img/image-small-400.[ext] 400w"', () => {
+        expect(sourceElWebp.attributes('srcset')).toEqual(
+          '/img/image-small.webp, /img/image-small-200.webp 200w, /img/image-small-400.webp 400w'
+        )
+        expect(sourceElPng.attributes('srcset')).toEqual(
+          '/img/image-small.png, /img/image-small-200.png 200w, /img/image-small-400.png 400w'
+        )
+        expect(sourceElJpg.attributes('srcset')).toEqual(
+          '/img/image-small.jpg, /img/image-small-200.jpg 200w, /img/image-small-400.jpg 400w'
+        )
+      })
+
+      it('Should have media attribute eq "(min-width: 300px) and (orientation: portrait)"', () => {
+        expect(sourceElWebp.attributes('media')).toEqual(
+          '(min-width: 300px) and (orientation: portrait)'
+        )
+        expect(sourceElPng.attributes('media')).toEqual(
+          '(min-width: 300px) and (orientation: portrait)'
+        )
+        expect(sourceElJpg.attributes('media')).toEqual(
+          '(min-width: 300px) and (orientation: portrait)'
+        )
+      })
+    })
+
+    describe('Testing rendering for "custom_size" image', () => {
+      let sourceElWebp: Wrapper<Vue>
+      let sourceElPng: Wrapper<Vue>
+      let sourceElJpg: Wrapper<Vue>
+
+      beforeEach(() => {
+        sourceElWebp = cmp.find(
+          'source[srcset*="/img/image-custom_size-100.webp 100w"]'
+        )
+        sourceElPng = cmp.find(
+          'source[srcset*="/img/image-custom_size-100.png 100w"]'
+        )
+        sourceElJpg = cmp.find(
+          'source[srcset*="/img/image-custom_size-100.jpg 100w"]'
+        )
+      })
+
+      it('Should render only png image', () => {
+        expect(sourceElWebp.exists()).toBeFalsy()
+        expect(sourceElJpg.exists()).toBeFalsy()
+        expect(sourceElPng.exists()).toBeTruthy()
+      })
+
+      it('Should have srcset eq "/img/image-custom_size.png, /img/image-custom_size-100.png 100w, /img/image-custom_size-300.png 300w"', () => {
+        expect(sourceElPng.attributes('srcset')).toEqual(
+          '/img/image-custom_size.png, /img/image-custom_size-100.png 100w, /img/image-custom_size-300.png 300w'
+        )
+      })
+
+      it('Should have "size" attribute eq to "(max-width: 300px) 50em, 40em, (min-width: 20px) 10vw', () => {
+        expect(sourceElPng.attributes('size')).toEqual(
+          '(max-width: 300px) 50em, 40em, (min-width: 20px) 10vw'
+        )
+      })
+    })
+  })
+
+  describe('Testing rendering without settings', () => {
+    let sourceElWebp: Wrapper<Vue>
+    let sourceElPng: Wrapper<Vue>
+    let sourceElJpg: Wrapper<Vue>
+
+    beforeEach(() => {
+      cmp = shallowMount(VuePictureElement, {
+        propsData: {
+          alt: 'Some alt text',
+          extensions: ['webp', 'png', 'jpg'],
+          path: '/img',
+          name: 'image'
+        }
+      })
+
+      sourceElWebp = cmp.find('source[src*="/img/image.webp"]')
+      sourceElPng = cmp.find('source[src*="/img/image.png"]')
+      sourceElJpg = cmp.find('source[src*="/img/image.jpg"]')
+    })
+
+    it('Should have src eq "/img/image.[ext]"', () => {
+      expect(sourceElWebp.attributes('src')).toEqual('/img/image.webp')
+      expect(sourceElPng.attributes('src')).toEqual('/img/image.png')
+      expect(sourceElJpg.attributes('src')).toEqual('/img/image.jpg')
     })
   })
 })
